7b3a9d0ab3caf488b50096fafc7bf533ba610a97
Rishabh Shukla <rishabhshukla1333@gmail.com>
2024-04-05 02:00:53 +0530

    root changes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # get 'students/new'
 
   resources:students
-  
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -13,5 +13,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "students#index"
 end
